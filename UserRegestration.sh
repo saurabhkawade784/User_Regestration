e79ec19f2acfcb67e0046fa53186618d22d78904
saurabhkawade784 <saurabhkawade784@gmail.com>
2020-09-27 13:25:04 +0530

    Validate User first name

--- UserRegestration.sh
+++ UserRegestration.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 #This program for validating user registration process.
 echo "Welcome to user registration process."
+read -p "Please enter your first name: " Name
+#Validate user first name.
+pat="^[[:upper:]]{1}[a-z A-Z 0-9]{2}$"
+if [[ $Name =~ $pat ]]
+then
+	echo "Valid_name"
+else
+	echo "Invalid_name"
+fi
